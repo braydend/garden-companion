786236b741e47f5fdb261c3b8c4051a2fcf92700
Brayden <braydendiluca@gmail.com>
2022-06-28 11:19:58 +0930

    Generate views for plants

---
+++ api/src/graphql/plants.sdl.ts
@@ -0,0 +1,28 @@
+export const schema = gql`
+  type Plant {
+    id: String!
+    name: String!
+    notes: String
+  }
+
+  type Query {
+    plants: [Plant!]! @requireAuth
+    plant(id: String!): Plant @requireAuth
+  }
+
+  input CreatePlantInput {
+    name: String!
+    notes: String
+  }
+
+  input UpdatePlantInput {
+    name: String
+    notes: String
+  }
+
+  type Mutation {
+    createPlant(input: CreatePlantInput!): Plant! @requireAuth
+    updatePlant(id: String!, input: UpdatePlantInput!): Plant! @requireAuth
+    deletePlant(id: String!): Plant! @requireAuth
+  }
+`
