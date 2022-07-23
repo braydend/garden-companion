869a5f095909fb66fdaa22b8fbd6d27c4fbc6f44
braydend <braydendiluca@gmail.com>
2022-07-23 17:02:02 +1000

    [WIP] Add Searchbox to PlantsList

--- web/src/components/organisms/Plant/PlantsList/PlantsList.tsx
+++ web/src/components/organisms/Plant/PlantsList/PlantsList.tsx
@@ -1,4 +1,8 @@
+import { useState } from 'react'
+
 import { Link, routes } from '@redwoodjs/router'
+
+import { SearchBox } from 'src/components/atoms/SearchBox'
 
 const MAX_STRING_LENGTH = 150
 
@@ -11,30 +15,45 @@ const truncate = (text) => {
 }
 
 export const PlantsList = ({ plants }) => {
+  const [filteredPlants, setFilteredPlants] = useState(plants)
+
   return (
-    <div className="rw-segment rw-table-wrapper-responsive">
-      <table className="rw-table">
-        <thead>
-          <tr>
-            <th>Name</th>
-          </tr>
-        </thead>
-        <tbody>
-          {plants.map((plant) => (
-            <tr key={plant.id}>
-              <td>
-                <Link
-                  to={routes.plant({ id: plant.id })}
-                  title={'Show plant ' + plant.id + ' detail'}
-                  className="hover:underline"
-                >
-                  {truncate(plant.name)}{' '}
-                </Link>
-              </td>
+    <>
+      <SearchBox
+        className={'w-full'}
+        placeholder={'Search for a plant...'}
+        onSearch={(query) =>
+          setFilteredPlants(
+            plants.filter(({ name }) =>
+              name.toLowerCase().includes(query.toLowerCase())
+            )
+          )
+        }
+      />
+      <div className={'w-full border-2 border-gray-300 rounded-md mt-2'}>
+        <table className="table-auto w-full">
+          <thead className={'bg-gray-200'}>
+            <tr className={'border-b-2 border-gray-300'}>
+              <th>Name</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+          </thead>
+          <tbody>
+            {filteredPlants.map((plant) => (
+              <tr key={plant.id}>
+                <td className={'border-b-2 p-2'}>
+                  <Link
+                    to={routes.plant({ id: plant.id })}
+                    title={'Show plant ' + plant.id + ' detail'}
+                    className="hover:underline"
+                  >
+                    {truncate(plant.name)}{' '}
+                  </Link>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
   )
 }
